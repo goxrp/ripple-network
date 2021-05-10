d7556db67eb0693686b80ff7e8135dabdd7b0c8a
John Wang <johncwang@gmail.com>
2021-05-10 00:37:11 -0700

    feat: gateways: add `gateways.json` data file

---
+++ cmd/build_gateways_data/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	ripplenetwork "github.com/go-xrp/ripple-network"
+)
+
+func main() {
+	gateways := ripplenetwork.Gateways()
+	bytes, err := json.MarshalIndent(gateways, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = ioutil.WriteFile("gateways.json", bytes, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("DONE")
+}
--- gateways.go
+++ gateways.go
@@ -8,12 +8,17 @@ type Account struct {
 	CoinistURL string `json:"coinistURL,omitempty"`
 }
 
-// https://bitcoin.stackexchange.com/questions/16936/is-there-a-comprehensive-list-of-gateways-ious-and-fees-associated-with-using-t#:~:text=There%20is%20no%20official%20list,any%20kind%20of%20comprehensive%20list.
+// https://bitcoin.stackexchange.com/questions/16936/is-there-a-comprehensive-list-of-gateways-ious-and-fees-associated-with-using-t
 // https://bitcointalk.org/index.php?topic=155236.msg1646402#msg1646402
 // https://coinist.co/ripple/gateways/
 
-func getAccounts() []Account {
+func Gateways() []Account {
 	return []Account{
+		{
+			Name:    "Bitstamp",
+			Address: "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+			URL:     "https://www.bitstamp.net/",
+		},
 		{
 			Name:       "Coinex",
 			Address:    "rsP3mgGb2tcYUrxiLFiHJiQXhsziegtwBc",
@@ -28,9 +33,39 @@ func getAccounts() []Account {
 			FeesURL: "https://www.dividendrippler.com/fees",
 		},
 		{
+			Name:    "Justcoin",
+			Address: "rJHygWcTLVpSXkowott6kzgZU6viQSVYM1",
+			URL:     "https://justcoin.com/",
+		},
+		{
+			Name:    "Peercover",
+			Address: "ra9eZxMbJrUcgV8ui7aPc161FgrqWScQxV",
+			URL:     "https://www.peercover.com/",
+		},
+		{
+			Name:    "Ripple Israel",
+			Address: "rNPRNzBB92BVpAhhZr4iXDTveCgV5Pofm9",
+			URL:     "http://rippleisrael.co.il/",
+		},
+		{
+			Name:    "Ripple Union",
+			Address: "r3ADD8kXSUKHd6zTCKfnKT3zV9EZHjzp1S",
+			URL:     "http://rippleunion.com/",
+		},
+		{
 			Name:    "RippleChina",
 			Address: "razqQKzJRdB4UxFPWf5NEpEG3WMkmwgcXA",
 			URL:     "http://wg.iripplechina.com/",
+		},
+		{
+			Name:    "RippleFund",
+			Address: "rE7CNMbxwvTQrqSEjbcXCrqeN6EZga1ApU",
+			URL:     "http://ripplefund.me/",
+		},
+		{
+			Name:    "TTBit",
+			Address: "rGwUWgN5BEg3QGNY3RX2HfYowjUTZdid3E",
+			URL:     "https://bitcointalk.org/index.php?topic=149533.0",
 		},
 		{
 			Name:    "WisePass",
