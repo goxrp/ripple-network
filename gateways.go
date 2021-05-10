2cc4cc6bb5529fe2a6b1df74bc3aee7a20442bce
John Wang <johncwang@gmail.com>
2021-05-10 00:46:29 -0700

    data: add gateways

--- gateways.go
+++ gateways.go
@@ -1,11 +1,12 @@
 package ripplenetwork
 
 type Account struct {
-	Name       string `json:"name,omitempty"`
-	Address    string `json:"address,omitempty"`
-	URL        string `json:"url,omitempty"`
-	FeesURL    string `json:"feesURL,omitempty"`
-	CoinistURL string `json:"coinistURL,omitempty"`
+	Name        string `json:"name,omitempty"`
+	Description string `json:"description,omitempty"`
+	Address     string `json:"address,omitempty"`
+	URL         string `json:"url,omitempty"`
+	FeesURL     string `json:"feesURL,omitempty"`
+	CoinistURL  string `json:"coinistURL,omitempty"`
 }
 
 // https://bitcoin.stackexchange.com/questions/16936/is-there-a-comprehensive-list-of-gateways-ious-and-fees-associated-with-using-t
@@ -31,6 +32,11 @@ func Gateways() []Account {
 			Address: "rfYv1TXnwgDDK4WQNbFALykYuEBnrR4pDX",
 			URL:     "https://www.dividendrippler.com/",
 			FeesURL: "https://www.dividendrippler.com/fees",
+		},
+		{
+			Name:    "ICE",
+			Address: "r4H3F9dDaYPFwbrUsusvNAHLz2sEZk4wE5",
+			URL:     "https://ripple.com/forum/viewtopic.php?f=1&t=3958",
 		},
 		{
 			Name:    "Justcoin",
@@ -63,6 +69,12 @@ func Gateways() []Account {
 			URL:     "http://ripplefund.me/",
 		},
 		{
+			Name:        "SnapSwap",
+			Description: "https://btc2ripple.com/",
+			Address:     "rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q",
+			URL:         "https://www.snapswap.us/",
+		},
+		{
 			Name:    "TTBit",
 			Address: "rGwUWgN5BEg3QGNY3RX2HfYowjUTZdid3E",
 			URL:     "https://bitcointalk.org/index.php?topic=149533.0",
@@ -71,6 +83,12 @@ func Gateways() []Account {
 			Name:    "WisePass",
 			Address: "rPDXxSZcuVL3ZWoyU82bcde3zwvmShkRyF",
 			URL:     "https://wisepass.com/",
+		},
+		{
+			Name:        "XRP Interchange",
+			Description: "deweller/wellerco https://xrptalk.org/user/803-deweller/, https://bitcointalk.org/index.php?action=profile;u=155537",
+			Address:     "rEUNavLM2NPpLW8dKqk3BRhXcZk5Hv7dhj",
+			URL:         "http://xrpio.com/",
 		},
 	}
 }
