ed071fb525ddcb2c6ffca16b2b174ab63dfb16dd
John Wang <johncwang@gmail.com>
2021-05-13 23:48:07 -0700

    feat: add gateways.csv table

--- cmd/build_gateways_data/main.go
+++ cmd/build_gateways_data/main.go
@@ -5,9 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"strconv"
+	"strings"
 	"time"
 
 	ripplenetwork "github.com/goxrp/ripple-network"
+	"github.com/grokify/gocharts/data/table"
+	"github.com/grokify/simplego/type/stringsutil"
 )
 
 func main() {
@@ -34,5 +38,29 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	tbl := table.NewTable()
+	tbl.Columns = []string{
+		"Name", "URL", "Address", "TransferRate", "CurrenciesReceive", "CurrenciesSend", "TimeRetrieved",
+	}
+
+	for _, gw := range gateways {
+		row := []string{
+			gw.Name,
+			gw.URL,
+			gw.Address,
+			strconv.Itoa(int(gw.AccountRoot.TransferRate)),
+			strings.Join(stringsutil.SliceCondenseSpace(gw.AccountCurriencies.ReceiveCurrencies, true, true), ","),
+			strings.Join(stringsutil.SliceCondenseSpace(gw.AccountCurriencies.SendCurrencies, true, true), ","),
+			gwSet.TimeCreated.Format(time.RFC3339),
+		}
+		tbl.Records = append(tbl.Records, row)
+	}
+
+	err = tbl.WriteCSV("gateways.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("DONE")
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/getkin/kin-openapi v0.60.0
 	github.com/goxrp/go-rippled v0.8.0
+	github.com/grokify/gocharts v1.3.0
 	github.com/grokify/simplego v0.26.2
 
 )
